439acf0813ebaa82abac78a419ba0f4c83f6d898
Maryam <maryam.zafar@devsinc.com>
2022-10-24 17:32:37 +0500

    Added lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,52 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import Layout from "./components/layout/Layout";
-import Login from "./pages/Auth/Login";
-import Signup from "./pages/Auth/Signup";
-import Home from "./pages/Home";
-
 import "bootstrap/dist/css/bootstrap.min.css";
-import NewPost from "./pages/Posts/NewPost";
-import EditPost from "./pages/Posts/EditPost";
-import SinglePost from "./pages/Posts/SinglePost";
+
+import LoadingSpinner from "./components/ui/LoadingSpinner";
+import Layout from "./components/layout/Layout";
 import PrivateRoute from "./PrivateRoute";
+
+const Login = React.lazy(() => import("./pages/Auth/Login"));
+const Signup = React.lazy(() => import("./pages/Auth/Signup"));
+const Home = React.lazy(() => import("./pages/Home"));
+const NewPost = React.lazy(() => import("./pages/Posts/NewPost"));
+const EditPost = React.lazy(() => import("./pages/Posts/EditPost"));
+const SinglePost = React.lazy(() => import("./pages/Posts/SinglePost"));
 
 const App = () => {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/signup">
-          <Signup />
-        </Route>
-        <PrivateRoute path="/new-post">
-          <NewPost />
-        </PrivateRoute>
-        <PrivateRoute path="/posts/:postId" exact>
-          <SinglePost />
-        </PrivateRoute>
-        <PrivateRoute path="/posts/:postId/edit">
-          <EditPost />
-        </PrivateRoute>
-        <Route path="*">
-          <Redirect to="/" />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <PrivateRoute path="/new-post">
+            <NewPost />
+          </PrivateRoute>
+          <PrivateRoute path="/posts/:postId" exact>
+            <SinglePost />
+          </PrivateRoute>
+          <PrivateRoute path="/posts/:postId/edit">
+            <EditPost />
+          </PrivateRoute>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 };
